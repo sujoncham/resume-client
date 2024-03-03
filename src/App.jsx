ac74ec944e46925aad2524ced9750a4c29004910
sujon-chambugong <sujonchambugong@gmail.com>
2024-03-03 17:32:56 +0600

    login, register and logout system done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import {
+  Route,
+  Routes,
+} from "react-router-dom/dist/umd/react-router-dom.development";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div>
       <Header />
       <div>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
       </div>
       <Footer />
     </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom/dist/umd/react-router-dom.development";
 import "../Header/Header.css";
-
 const Header = () => {
   const [open, setOpen] = useState(true);
+  const user = localStorage.getItem("token");
+  const navigate = useNavigate();
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
     if (section) {
@@ -10,33 +13,59 @@ const Header = () => {
     }
   };
 
+  const handleLogout = () => {
+    localStorage.removeItem("userId");
+    localStorage.removeItem("token");
+    navigate("/");
+  };
+
   const menu = (
     <>
       <li className="hover:bg-purple-400 px-2 py-1 rounded-md">
-        <a href="#banner" onClick={() => scrollToSection("banner")}>
+        <Link href="#banner" onClick={() => scrollToSection("banner")}>
           Home
-        </a>
+        </Link>
       </li>
       <li className="hover:bg-purple-400 px-2 py-1 rounded-md">
-        <a href="#introduce" onClick={() => scrollToSection("introduce")}>
+        <Link href="#introduce" onClick={() => scrollToSection("introduce")}>
           About
-        </a>
+        </Link>
       </li>
       <li className="hover:bg-purple-400 px-2 py-1 rounded-md">
-        <a href="#mySkills" onClick={() => scrollToSection("mySkills")}>
+        <Link href="#mySkills" onClick={() => scrollToSection("mySkills")}>
           Skills
-        </a>
+        </Link>
       </li>
       <li className="hover:bg-purple-400 px-2 py-1 rounded-md">
-        <a href="#projects" onClick={() => scrollToSection("projects")}>
+        <Link href="#projects" onClick={() => scrollToSection("projects")}>
           Projects
-        </a>
+        </Link>
       </li>
       <li className="hover:bg-purple-400 px-2 py-1 rounded-md">
-        <a href="#contactForm" onClick={() => scrollToSection("contactForm")}>
+        <Link
+          href="#contactForm"
+          onClick={() => scrollToSection("contactForm")}
+        >
           Contact
-        </a>
+        </Link>
       </li>
+      {!user ? (
+        <li className="hover:bg-purple-400 px-2 py-1 rounded-md">
+          <Link to={"/login"}>Login</Link>
+        </li>
+      ) : (
+        ""
+      )}
+      {user && (
+        <>
+          <li className="hover:bg-purple-400 px-2 py-1 rounded-md">
+            <Link to={"/register"}>Register</Link>
+          </li>
+          <li className="hover:bg-purple-400 px-2 py-1 rounded-md">
+            <button onClick={handleLogout}>Logout</button>
+          </li>
+        </>
+      )}
     </>
   );
 
@@ -48,15 +77,18 @@ const Header = () => {
             Logo
           </a>
           <div className="xs:hidden sm:hidden md:block lg:block">
-            <ul className="flex justify-start items-center gap-2">{menu}</ul>
+            <ul className="flex justify-start items-center gap-2 md:text-white lg:text-white">
+              {menu}
+            </ul>
           </div>
-          <div className="xs:block sm:block md:hidden lg:hidden">
+          <div className="xs:block sm:block md:hidden lg:hidden text-white">
             {open && <button onClick={() => setOpen(!open)}>|||</button>}
             {!open && <button onClick={() => setOpen(!open)}>X</button>}
           </div>
+          <div></div>
           {!open && (
             <div className="absolute z-50 top-10 bg-black opacity-70 w-full">
-              <ul className="md:hidden lg:hidden xs:flex-col sm:flex-col justify-start items-center gap-2 text-white">
+              <ul className="md:hidden lg:hidden xs:flex-col sm:flex-col justify-start items-center gap-2 xs:text-white sm:text-white">
                 {menu}
               </ul>
             </div>
